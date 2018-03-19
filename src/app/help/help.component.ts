e05bf62acc1cfd6c66855252d3ef1e1e091ad062
Anastasis Georgoulas <a.georgoulas@ucl.ac.uk>
2018-03-19 14:14:09 +0000

    Add component for showing help
    Currently just displays the text in a separate page. The table is
    also displayed, because it's in the main component.

---
+++ src/app/help/help.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'help',
+  templateUrl: './help.component.html',
+//  styleUrls: ['./help.component.css'],
+  moduleId: module.id,
+})
+
+export class HelpComponent {
+  title = 'help page';
+  text = 'This is some help content';
+}
