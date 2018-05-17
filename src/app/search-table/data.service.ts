2afc85f278e32ad86090f98cec6139d0419e7237
Anastasis Georgoulas <a.georgoulas@ucl.ac.uk>
2018-05-17 12:12:25 +0100

    Treat counts as numbers
    This converts the icount field to an integer, rather than text.
    Ideally this will be done at the back-end, but I'm not sure that
    the JSON will be transferred as anything other than text. In the
    meantime, this will do (the code could probably also be made more
    concise). This also makes the results table field for counts
    right-aligned.

--- src/app/search-table/data.service.ts
+++ src/app/search-table/data.service.ts
@@ -13,7 +13,15 @@ export class DataService {
   getEntries() {
       //return this.http.get("http://localhost:5000/search_all")
       return this.http.get("http://localhost:3000/entries")
-          .map(res => res.json());
+          .map(function(res) {
+            let json = res.json();
+            // is this more readable?
+            //json.map(e => {e.icount = parseInt(e.icount); return e});
+            for (var entry of json) {
+              entry.icount = parseInt(entry.icount);
+            }
+            return json
+          });
 //    return [
 //        {"gw": "\u02beAtean", "headword": "\u02beAtaya[\u02beAtean]EN", "cf": "\u02beAtaya"},
 //        {"gw": "the sign A\u2082", "headword": "a[the sign A\u2082]N", "cf": "a"},
