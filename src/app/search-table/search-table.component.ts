074b6c5ab4fa2c475b9b061f17dd90d007f8cb4e
Anastasis Georgoulas <a.georgoulas@ucl.ac.uk>
2018-05-17 12:06:06 +0100

    Define custom sorting for table entries
    Apparently this is the preferred way, and it will let us do more
    customised sorting (to account for character variants, fragemented
    words etc) later.

--- src/app/search-table/search-table.component.ts
+++ src/app/search-table/search-table.component.ts
@@ -37,5 +37,16 @@ export class SearchTableComponent {
     this.key = key;
     this.reverse = !this.reverse;
   }
+  sortedEntries(): any {
+    return this.entries.sort((a, b) => {
+      let a_val = a[this.key];
+      let b_val = b[this.key];
+      if this.reverse {
+        return (a_val < b_val) ? -1 : ((a_val == b_val) ? 0 : 1)
+      } else {
+        return (a_val > b_val) ? -1 : ((a_val == b_val) ? 0 : 1)
+      }
+    })
+  }
   p: number = 1;
 }
