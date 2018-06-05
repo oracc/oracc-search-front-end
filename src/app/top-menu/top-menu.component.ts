fb65a950fbfc48c841ebd1aac5ce92bf99e6529f
Anastasis Georgoulas <a.georgoulas@ucl.ac.uk>
2018-06-05 16:52:17 +0100

    Make menu prettier

--- src/app/top-menu/top-menu.component.ts
+++ src/app/top-menu/top-menu.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'top-menu',
   templateUrl: './top-menu.component.html',
-  styleUrls: ['../app.component.css'],
+  styleUrls: ['./top-menu.component.css', '../app.component.css'],
   moduleId: module.id,
 })
 
