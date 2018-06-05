d3c47969b15552e0885617359a11a49e4b500304
Anastasis Georgoulas <a.georgoulas@ucl.ac.uk>
2018-06-05 16:10:45 +0100

    Add basic support for mobile

--- src/app/top-menu/top-menu.component.ts
+++ src/app/top-menu/top-menu.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'top-menu',
   templateUrl: './top-menu.component.html',
-//  styleUrls: ['./app.component.css'],
+  styleUrls: ['../app.component.css'],
   moduleId: module.id,
 })
 
