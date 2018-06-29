5fc99620bd12be31e0e3d3c033b9c5c27360d29f
Anastasis Georgoulas <a.georgoulas@ucl.ac.uk>
2018-06-29 11:59:41 +0100

    Add some help content

--- src/app/translate/lang-en.ts
+++ src/app/translate/lang-en.ts
@@ -9,7 +9,19 @@ export const LANG_EN_TRANS = {
     'search.periods': 'periods',
     'search.meanings': 'meanings',
     // Translations for the help page
-    'help.text': 'This is some help content',
+    'help.title': 'Help',
+    'help.intro': 'You can search the contents of ORACC in two different ways:',
+    'help.heading1': 'Search the ORACC entries',
+    'help.text1': `You can query the corpus of transcriptions with a query of your choice. Your query will be
+      matched against both the original text and its translated meaning. Keep in mind that you
+      can also use extended Unicode characters such as ā or š (for more information on how to
+      enter these, click here).
+      In the list of returned results, you can find include links to the occurunces of each word
+      that matched your query.`,
+    'help.heading2': 'Search the ORACC information pages (coming soon)',
+    'help.text2': `In addition to transcriptions, the ORACC project also comprises a wealth of information about
+      Mesopotamian history and culture, which is searchable through this website. Your query will
+      return links to relevant queries, which you can then visit by clicking on the links displayed.`,
     // Translations for the home page
     'welcome.title': 'Welcome to ORACC\'s website!',
     'welcome.text': `Oracc is a collaborative effort to develop a complete corpus of cuneiform whose rich annotation
