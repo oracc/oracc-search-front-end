daaede822a98315ebd55e33c04a5305b58791e6e
Anastasis Georgoulas <a.georgoulas@ucl.ac.uk>
2018-07-16 13:42:13 +0100

    Correct capitalisation of Oracc

--- src/app/translate/lang-en.ts
+++ src/app/translate/lang-en.ts
@@ -10,20 +10,20 @@ export const LANG_EN_TRANS = {
     'search.meanings': 'meanings',
     // Translations for the help page
     'help.title': 'Help',
-    'help.intro': 'You can search the contents of ORACC in two different ways:',
-    'help.heading1': 'Search the ORACC entries',
+    'help.intro': 'You can search the contents of Oracc in two different ways:',
+    'help.heading1': 'Search the Oracc entries',
     'help.text1': `You can query the corpus of transcriptions with a query of your choice. Your query will be
       matched against both the original text and its translated meaning. Keep in mind that you
       can also use extended Unicode characters such as ā or š (for more information on how to
       enter these, click here).
       In the list of returned results, you can find include links to the occurunces of each word
       that matched your query.`,
-    'help.heading2': 'Search the ORACC information pages (coming soon)',
-    'help.text2': `In addition to transcriptions, the ORACC project also comprises a wealth of information about
+    'help.heading2': 'Search the Oracc information pages (coming soon)',
+    'help.text2': `In addition to transcriptions, the Oracc project also comprises a wealth of information about
       Mesopotamian history and culture, which is searchable through this website. Your query will
       return links to relevant queries, which you can then visit by clicking on the links displayed.`,
     // Translations for the home page
-    'welcome.title': 'Welcome to ORACC\'s website!',
+    'welcome.title': 'Welcome to the Oracc website!',
     'welcome.text': `Oracc is a collaborative effort to develop a complete corpus of cuneiform whose rich annotation
       and open licensing support the next generation of scholarly research. Created by Steve Tinney,
       Oracc is steered by Jamie Novotny, Eleanor Robson, Tinney, and Niek Veldhuis.`,
