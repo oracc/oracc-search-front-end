5d6da76d1168250c1bf34f1bac77b7fb9450bfe1
Anastasis Georgoulas <a.georgoulas@ucl.ac.uk>
2018-08-30 14:27:59 +0100

    Remove unnecessary method

--- src/app/display-table/display-table.component.ts
+++ src/app/display-table/display-table.component.ts
@@ -43,17 +43,7 @@ export class DisplayTableComponent {
     });
 
   }
-  sortedEntries(): any {
-    return this.entries.sort((a, b) => {
-      let a_val = a[this.key];
-      let b_val = b[this.key];
-      if (this.reverse) {
-        return (a_val < b_val) ? -1 : ((a_val == b_val) ? 0 : 1)
-      } else {
-        return (a_val > b_val) ? -1 : ((a_val == b_val) ? 0 : 1)
-      }
-    })
-  }
+
   p: number = 1;
 
   // clicking and hightling rows on mouseover
