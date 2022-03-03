fa14af3cafd9eccbc30bc5d3c156e2d65a8097be
James Hughes <70143648+jhughes982@users.noreply.github.com>
2022-03-03 10:34:10 +0000

    Update src/app/components/cookies/cookies.component.ts
    Co-authored-by: ageorgou <1186102+ageorgou@users.noreply.github.com>

--- src/app/components/cookies/cookies.component.ts
+++ src/app/components/cookies/cookies.component.ts
@@ -12,7 +12,7 @@ import {
   styleUrls: ["./cookies.component.scss"]
 })
 export class CookiesComponent implements OnInit {
-  public isHidden = isDevMode() ? true : false;
+  public isHidden = isDevMode();
   @Output() public cookieHiddenEvent = new EventEmitter();
 
   constructor() {}
