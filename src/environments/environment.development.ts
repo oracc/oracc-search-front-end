9de694bc21cfaad9be3cdd95e2a98279c9550e41
acholyn <a.ho-lyn@ucl.ac.uk>
2024-04-05 13:39:10 +0100

    fixing + tidying search

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:8000',
-  glossaryArticleURL: 'http://localhost:8000'
+  glossaryArticleURL: 'https://build-oracc.museum.upenn.edu'
 };
