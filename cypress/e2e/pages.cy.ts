41f48660d464654e5cc9d79578d3b82a61a35439
Tim Band <t.band@nnnn.com>
2024-07-23 12:42:27 +0100

    E2E test backend stub code fixed

--- cypress/e2e/pages.cy.ts
+++ cypress/e2e/pages.cy.ts
@@ -1,84 +1,68 @@
+import { CyHttpMessages, RouteMatcher, RouteMatcherOptionsGeneric } from "cypress/types/net-stubbing";
+
 class MockResponse {
   constructor(public body_fixture_path: string) {}
 }
 
+type InterceptData = {
+  response: CyHttpMessages.IncomingHttpResponse;
+  routeMatcher: RouteMatcher;
+}
+
 describe('Page', () => {
-  let searches = [];
-  let suggests = [];
-  let oracc_calls = [];
+  let searches: InterceptData[] = [];
+  let suggests: InterceptData[] = [];
+  let oracc_calls: InterceptData[] = [];
   // These generic intercepts need to be declared before the generic
   // ones or they won't run.
   beforeEach(() => {
-    cy.intercept('GET', '/search/*', (req) => {
+    cy.intercept('GET', 'http://localhost:8000/search/*', (req) => {
       req.continue((res) => {
-        searches.push([req['url'], req['query'], res]);
+        searches.push(interceptData(req, res));
       });
     });
-    cy.intercept('GET', '/suggest_all/*', (req) => {
+    cy.intercept('GET', 'http://localhost:8000/suggest_all/*', (req) => {
       req.continue((res) => {
-        suggests.push([req['url'], req['query'], res]);
+        suggests.push(interceptData(req, res));
       });
     });
     cy.intercept('GET', 'https://build-oracc.museum.upenn.edu/**', (req) => {
       req.continue((res) => {
-        oracc_calls.push([req['url'], req['query'], res]);
+        oracc_calls.push(interceptData(req, res));
       });
     });
   });
   beforeEach(() => {
     cy.fixture('directory.json').then((dir) => {
-      // href2query2response[path][queryString] gives the response to href?queryString
-      // queryString is normalized so that the portions in between the &s are sorted.
-      let href2query2response = new Map<string, Map<string, MockResponse>>();
-      Object.keys(dir).forEach((path) => {
-        const url = new URL(dir[path]);
-        const hpath = `${url.protocol}//${url.host}${url.pathname}`;
-        // normalise the search params by sorting them
-        url.searchParams.sort()
-        const query = url.searchParams.toString();
-        if (!href2query2response.has(hpath)) {
-          href2query2response.set(hpath, new Map<string, MockResponse>());
-        }
-        href2query2response.get(hpath).set(query, new MockResponse(path));
-      });
-      href2query2response.forEach((mr_map, hpath) => {
-        cy.intercept(hpath, (req) => {
-          let params = Object.keys(req.query);
-          params.sort();
-          const qps = params.map((param) => `${param}=${req.query[param]}`);
-          const query = qps.join("&");
-          if (mr_map.has(query)) {
-            req.reply({ fixture: mr_map.get(query).body_fixture_path });
-          }
+      Object.keys(dir).forEach((fixture_path) => {
+        cy.intercept(dir[fixture_path], (req) => {
+          req.reply({ fixture: fixture_path });
         });
       });
     });
   });
   after(() => {
-    let directory = new Map<string, string>();
     const output_dir = 'cypress/fixtures_new';
-    let count = 0;
-    count += writeCapturedResponses(
+    const searches_dir = writeCapturedResponses(
       searches,
       output_dir,
-      'backend',
-      (path, url) => { directory.set(path, url); }
+      'backend'
     );
-    count += writeCapturedResponses(
+    const suggests_dir = writeCapturedResponses(
       suggests,
       output_dir,
-      'suggests',
-      (path, url) => { directory.set(path, url); }
+      'suggests'
     );
-    count += writeCapturedResponses(
+    const oracc_dir = writeCapturedResponses(
       oracc_calls,
       output_dir,
-      'oracc',
-      (path, url) => { directory.set(path, url); }
+      'oracc'
     );
-    if (0 < count) {
-      cy.writeFile(`${output_dir}/directory.json`, Object.fromEntries(directory.entries()));
-    }
+    cy.writeFile(`${output_dir}/directory.json`, {
+      ...searches_dir,
+      ...suggests_dir,
+      ...oracc_dir
+    });
   });
 
   describe('home page', () => {
@@ -134,29 +118,43 @@ describe('Page', () => {
   });
 });
 
-// add_to_directory(path, url) is a callback for all the paths
-// that have been written to.
-function writeCapturedResponses(
-  captured: any[],
-  fixture_dir: string,
-  name: string,
-  add_to_directory: (path: string, url: string) => void
-): number {
-  let count = 0;
-  captured.forEach(([url_string, query, res]) => {
-    const loc: string = urlToFilePath(url_string, query, [name]);
-    add_to_directory(loc, url_string);
-    cy.writeFile(`${fixture_dir}/${loc}`, res['body']);
-    ++count;
-  });
-  return count;
+function interceptData(
+  req: CyHttpMessages.IncomingHttpRequest,
+  res: CyHttpMessages.IncomingHttpResponse
+): InterceptData {
+  const url = new URL(req.url);
+  return {
+    response: res,
+    routeMatcher: {
+      hostname: url.hostname,
+      pathname: url.pathname,
+      query: Object.fromEntries(url.searchParams.entries())
+    }
+  };
 }
 
-function urlToFilePath(url_string: any, query: any, initial: string[]) {
-  const url = new URL(url_string);
+function writeCapturedResponses(
+  captured: InterceptData[],
+  fixture_dir: string,
+  name: string
+): Map<string, RouteMatcher> {
+  let r = new Map<string, RouteMatcher>();
+  captured.forEach((intercept_data) => {
+    const loc: string = urlToFilePath(
+      (intercept_data.routeMatcher as RouteMatcherOptionsGeneric<string>).pathname,
+      (intercept_data.routeMatcher as RouteMatcherOptionsGeneric<string>).query,
+      [name]
+    );
+    cy.writeFile(`${fixture_dir}/${loc}`, intercept_data.response.body);
+    r[loc] = intercept_data.routeMatcher;
+  });
+  return r;
+}
+
+function urlToFilePath(pathname: string, query: any, initial: string[]) {
   const params = Object.keys(query).sort();
   let qs = params.map((param) => `${param}=${query[param]}`);
-  let path = url.pathname.split('/').slice(1); // initial '/' produces initial ''
+  let path = pathname.split('/').slice(1); // initial '/' produces initial ''
   let name = 'index';
   if (path.length) {
     name = path.pop();
