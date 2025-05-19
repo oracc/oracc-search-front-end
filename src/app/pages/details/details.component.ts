3c7f376df2a77dbbc785224e79d8bc352707d9a8
acholyn <a.ho-lyn@ucl.ac.uk>
2025-05-19 11:20:36 +0100

    adding scroll to selected + some styling (+ linting)

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,4 +1,10 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  ViewEncapsulation,
+  ElementRef,
+  ViewChild,
+  AfterViewInit
+} from '@angular/core';
 import { composedPath } from '../../../utils/utils';
 import { ThreePanel } from 'src/app/components/three-panel.component';
 import { mergeParams } from '../../../utils/utils';
@@ -9,8 +15,13 @@ import { mergeParams } from '../../../utils/utils';
   styleUrls: ['./details.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class DetailsComponent extends ThreePanel {
-  override setMiddlePanel(htmlData : Document) {
+export class DetailsComponent extends ThreePanel implements AfterViewInit {
+  @ViewChild('scrollContainer') private scrollContainer: ElementRef;
+  ngAfterViewInit(): void {
+    this.scrollToSelected();
+  }
+
+  override setMiddlePanel(htmlData: Document) {
     const middlePanelInput = htmlData.getElementById('p4Content');
 
     this.middlePanel = this.sanitizer.bypassSecurityTrustHtml(
@@ -27,7 +38,7 @@ export class DetailsComponent extends ThreePanel {
   }
 
   override detailsPanelTopText(): string {
-    return "details.linesText";
+    return 'details.linesText';
   }
 
   public handleDetailsClick(e) {
@@ -41,33 +52,46 @@ export class DetailsComponent extends ThreePanel {
         });
 
     const iref = anchorEl.getAttribute('data-iref');
-    const proj = anchorEl.hasAttribute('data-proj')?
-        anchorEl.getAttribute('data-proj') : this.project;
+    const proj = anchorEl.hasAttribute('data-proj')
+      ? anchorEl.getAttribute('data-proj')
+      : this.project;
 
     if (iref) {
       // navigates to details texts component
       this.router.navigate(
-        [ 'search-results',
-          this.chosenTermText,
-          'occurrences',
-          'texts'
-        ],
-        { queryParams: mergeParams(
-          {
-            proj: proj,
-            iref: iref,
-          },
-          this.route.snapshot.queryParams,
-          ['ga_lang', 'ga_isid', 'lang', 'isid', 'gw', 'type', 'name', 'pos']
-        )}
+        ['search-results', this.chosenTermText, 'occurrences', 'texts'],
+        {
+          queryParams: mergeParams(
+            {
+              proj: proj,
+              iref: iref
+            },
+            this.route.snapshot.queryParams,
+            ['ga_lang', 'ga_isid', 'lang', 'isid', 'gw', 'type', 'name', 'pos']
+          )
+        }
       );
     }
   }
 
   public handleMetadataClick(e) {
     e.preventDefault();
-    if (e.target.tagName === "A" && e.target.hasAttribute('data-zoom')) {
+    if (e.target.tagName === 'A' && e.target.hasAttribute('data-zoom')) {
       this.setZoom(parseInt(e.target.getAttribute('data-zoom'), 10));
     }
+  }
+
+  private scrollToSelected(): void {
+    // Being doubly sure the element should be rendered, might not need the timeout
+    setTimeout(() => {
+      const container = this.scrollContainer.nativeElement;
+      const selectedElement = container.querySelector('.selected');
+      if (selectedElement) {
+        selectedElement.scrollIntoView({
+          behavior: 'smooth',
+          block: 'nearest'
+        });
+      }
+    });
   }
 }
