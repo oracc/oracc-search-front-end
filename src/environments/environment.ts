3f731314460b838f2204830025368f7e555e88f2
Tim Band <t.b@ucl>
2025-07-23 14:57:43 +0100

    Removed github pages deploy In order to remove the last npm audit errors

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 // Production environment file
 
 // This file can be replaced during build by using the `fileReplacements` array.
-// `ng build -c github-pages` replaces `environment.ts` with `environment.staging.ts`.
+// `ng build -c build-oracc` replaces `environment.ts` with `environment.staging.ts`.
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
